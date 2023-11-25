dc785b040f18ee0be0bcbdba12907db78b4d5a7b
Compass System <bucketofcompasses@gmail.com>
2023-11-25 14:12:16 +0000

    Initial project layout

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.20"
+
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "compass_system.compass-plural-bot"
+version = "2.0.0"
+
+repositories {
+    google()
+    mavenCentral()
+
+    maven {
+        name = "Sonatype Snapshots (Legacy)"
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+
+    maven {
+        name = "Sonatype Snapshots"
+        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.7.0-SNAPSHOT")
+
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+
+	implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.11.1")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    jar {
+        manifest.attributes("Main-Class" to "compass_system.compass_plural_bot.MainKt")
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    withType<JavaExec> {
+        workingDir("run")
+    }
+}
+
+application {
+    mainClass.set("compass_system.compass_plural_bot.MainKt")
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Compass Plural Bot
+This is a personal plural bot for us, the Compasses.
+
+For more information about plurality please read https://morethanone.info/
